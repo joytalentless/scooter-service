1b59e8fefac265aca48c9c3bd939c9569a73b97c
WevTop <danieldent20@gmail.com>
2019-04-08 15:35:54 +0200

    Switch region to EU and replace Ramda with pure ES6

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as request from 'superagent';
-import * as R from 'ramda';
 
 const OSLO = {
     lat: 59.913868,
@@ -23,7 +22,8 @@ interface Voi {
     id: string,
     location: number[],
     short: string,
-    battery: number
+    battery: number,
+    zone: number
 }
 
 interface Tier {
@@ -36,30 +36,24 @@ interface Tier {
     }
 }
 
-export const nearbyVOI = functions.https.onRequest(async (req, res) => {
+export const nearbyVoi = functions.region('europe-west1').https.onRequest(async (req, res) => {
     try {
         const apiRes: request.Response = await request.get(
             `${voiApiUrl}?la=${OSLO.lat}&lo=${OSLO.long}`
         );
         const vehicles = JSON.parse(apiRes.text);
+        const vehiclesOslo = vehicles.filter((v: Voi) => v.zone === 27);
 
-        const oslo = R.filter(
-            (it: any) =>
-                R.contains('59.', R.toString(R.prop('location', it)[0])) &&
-                R.contains('10.', R.toString(R.prop('location', it)[1])),
-            vehicles
-        );
+        console.log(`Voi bikes in Oslo: ${vehiclesOslo.length}`);
 
-        console.log(`Voi bikes in Oslo ${R.length(oslo)}`);
-
-        res.status(200).send(oslo);
+        res.status(200).send(vehiclesOslo);
     } catch (e) {
         console.error(e);
         res.status(500).send(e);
     }
 });
 
-export const nearbyTier = functions.https.onRequest(async (req, res) => {
+export const nearbyTier = functions.region('europe-west1').https.onRequest(async (req, res) => {
     try {
         const apiRes: request.Response = await request
             .get(`${tierApiUrl}`)
@@ -75,19 +69,13 @@ export const nearbyTier = functions.https.onRequest(async (req, res) => {
     }
 });
 
-export const nearby = functions.https.onRequest(async (req, res) => {
+export const nearby = functions.region('europe-west1').https.onRequest(async (req, res) => {
     try {
         const voiResponse: request.Response = await request.get(
             `${voiApiUrl}?la=${OSLO.lat}&lo=${OSLO.long}`
         );
         const voiAll = JSON.parse(voiResponse.text);
-
-        const voi = R.filter(
-            (it: any) =>
-                R.contains('59.', R.toString(R.prop('location', it)[0])) &&
-                R.contains('10.', R.toString(R.prop('location', it)[1])),
-            voiAll
-        );
+        const voi = voiAll.filter((v: Voi) => v.zone === 27);
 
         const voiMapped: Vehicle[] = voi.map((v:Voi) => ({
             id: v.id,
@@ -113,7 +101,7 @@ export const nearby = functions.https.onRequest(async (req, res) => {
             battery: t.attributes.batteryLevel
         }));
 
-        const vehicles = R.union(voiMapped, tierMapped);
+        const vehicles = voiMapped.concat(tierMapped);
 
         console.log(`Scooters in Oslo: ${vehicles.length}`);
         res.status(200).send(vehicles);

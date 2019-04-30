cb5269e46926f56c4cb2b3cf892ccda9fea79e46
WevTop <danieldent20@gmail.com>
2019-04-30 15:04:05 +0200

    Serve prices for scooters and city bikes in cloud
    Also move content of index.ts to its own file

---
+++ functions/src/mobilityPrices.ts
@@ -0,0 +1,56 @@
+import * as functions from 'firebase-functions';
+
+interface priceText {
+    nob: string,
+    nno: string,
+    eng: string
+}
+
+interface priceTexts {
+    [key: string]: priceText
+}
+
+const mobilityPrices: priceTexts = {
+    voi: {
+        nob: '10 kr i oppstart + 1,50 kr per min',
+        nno: '10 kr i oppstart + 1,50 kr per min',
+        eng: 'NOK 10 to unlock + NOK 1,50 per min'
+    },
+    tier: {
+        nob: '10 kr i oppstart + 2 kr per min',
+        nno: '10 kr i oppstart + 2 kr per min',
+        eng: 'NOK 10 to unlock + NOK 2 per min'
+    }
+};
+
+const osloBergenTrondheim : priceText = {
+    nob: '399 kr for sesongspass / 49 kr for dagspass',
+    nno: '399 kr for sesongspass / 49 kr for dagspass',
+    eng: 'NOK 399 for season pass / NOK 49 for day pass'
+};
+
+const cityBikePrices: priceTexts = {
+    oslo: osloBergenTrondheim,
+    bergen: osloBergenTrondheim,
+    trondheim: osloBergenTrondheim,
+    drammen: {
+        nob: '130 kr for sesongspass',
+        nno: '130 kr for sesongpass',
+        eng: 'NOK 130 for season pass'
+    },
+    lillestrom: {
+        eng: 'NOK 50 for season pass / NOK 10 for 3-day pass',
+        nob: '50 kr for sesongpass / 10 kr for 3-dagerskort',
+        nno: 'NOK 50 for sesongpass / NOK 10 for 3-dagarskort'
+    }
+};
+
+export const scooterPriceText = functions.region('europe-west1').https
+    .onRequest(async (req, res) => {
+        res.status(200).send(mobilityPrices);
+    });
+
+export const cityBikePriceText = functions.region('europe-west1').https
+    .onRequest(async (req, res) => {
+        res.status(200).send(cityBikePrices)
+    });
---
+++ functions/src/scootersInMap.ts
@@ -0,0 +1,165 @@
+import * as functions from 'firebase-functions';
+import * as request from 'superagent';
+
+const OSLO = {
+    lat: 59.913868,
+    long: 10.752245
+};
+
+const voiApiUrl = 'https://api.voiapp.io/v1/vehicle/status/ready';
+const tierApiUrl = 'https://platform.tier-services.io/v1/vehicle?zoneId=OSLO';
+
+interface Vehicle {
+    id: string,
+    operator: string,
+    lat: number,
+    lon: number,
+    code: string,
+    battery: number
+}
+
+interface Voi {
+    id: string,
+    location: number[],
+    short: string,
+    battery: number,
+    zone: number
+}
+
+interface Tier {
+    id: string;
+    attributes: {
+        lat: number,
+        lng: number,
+        code: number,
+        batteryLevel: number
+    }
+}
+
+export const nearbyVoi = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    try {
+        const apiRes: request.Response = await request
+            .get(`${voiApiUrl}?la=${OSLO.lat}&lo=${OSLO.long}`
+            );
+        const vehicles = JSON.parse(apiRes.text);
+        const vehiclesOslo = vehicles.filter((v: Voi) => v.zone === 27);
+
+        console.log(`Voi bikes in Oslo: ${vehiclesOslo.length}`);
+
+        res.status(200).send(vehiclesOslo);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+export const nearbyTier = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    try {
+        const apiRes: request.Response = await request
+            .get(`${tierApiUrl}`)
+            .set('x-api-key', functions.config().tier.api.key);
+        const vehicles = JSON.parse(apiRes.text);
+
+        console.log(`Tier bikes in Oslo: ${vehicles.data.length}`);
+
+        res.status(200).send(vehicles);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+export const oslo = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    try {
+        const voiResponse: request.Response = await request
+            .get(`${voiApiUrl}?la=${OSLO.lat}&lo=${OSLO.long}`
+            );
+        const voiAll = JSON.parse(voiResponse.text);
+        const voi = voiAll.filter((v: Voi) => v.zone === 27);
+        const voiMapped: Vehicle[] = mapVoi(voi);
+
+        const tierResponse: request.Response = await request
+            .get(`${tierApiUrl}`)
+            .set('x-api-key', functions.config().tier.api.key);
+        const tier = JSON.parse(tierResponse.text).data;
+        const tierMapped: Vehicle[] = mapTier(tier);
+
+        const vehicles = voiMapped.concat(tierMapped);
+
+        console.log(`Scooters in Oslo: ${vehicles.length}`);
+        res.status(200).send(vehicles);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+export const nearby = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    const lat: number = req.query.lat;
+    const lon: number = req.query.lon;
+    const range: number = req.query.range || 100;
+    const max: number = req.query.max || 100;
+
+    if (lat === undefined || lon === undefined) {
+        res.status(422).send("Coordinates missing (lat and lon)");
+    }
+
+    try {
+        const voiResponse: request.Response = await request
+            .get(`${voiApiUrl}?la=${OSLO.lat}&lo=${OSLO.long}`
+            );
+        const voiAll = JSON.parse(voiResponse.text);
+        const voiOslo = voiAll.filter((v: Voi) => v.zone === 27);
+        const voi = voiOslo.filter((v: Voi) => distance(lat, lon, v.location[0], v.location[1]) < range);
+        const voiMapped: Vehicle[] = mapVoi(voi);
+
+        const tierResponse: request.Response = await request
+            .get(`${tierApiUrl}&lat=${lat}&lng=${lon}&radius=${range}`)
+            .set('x-api-key', functions.config().tier.api.key);
+        const tier = JSON.parse(tierResponse.text).data;
+        const tierMapped: Vehicle[] = mapTier(tier);
+
+        const vehicles = voiMapped.concat(tierMapped);
+        const closestVehicles = vehicles.sort((v1, v2) => {
+            return distance(lat, lon, v1.lat, v1.lon) - distance(lat, lon, v2.lat, v2.lon)
+        }).slice(0, max);
+
+        console.log(`Scooters nearby (${lat}, ${lon}, range: ${range}, max: ${max}): ${closestVehicles.length}`);
+        res.status(200).send(closestVehicles);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+function distance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const p = 0.017453292519943295;    // Math.PI / 180
+    const c = Math.cos;
+    const a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+        (1 - c((lon2 - lon1) * p)) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a)) * 1000; // 2 * R; R = 6371 km
+}
+
+function mapVoi(voiScooters: Voi[]): Vehicle[] {
+    return voiScooters.map((v: Voi) => ({
+        id: v.id,
+        operator: 'voi',
+        lat: v.location[0],
+        lon: v.location[1],
+        code: v.short,
+        battery: v.battery
+    }));
+}
+
+function mapTier(tierScooters: Tier[]): Vehicle[] {
+    return tierScooters.map((t: Tier) => ({
+        id: t.id,
+        operator: 'tier',
+        lat: t.attributes.lat,
+        lon: t.attributes.lng,
+        code: t.attributes.code.toString(),
+        battery: t.attributes.batteryLevel
+    }));
+}

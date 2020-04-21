9378fcac3600c6aee8ed3e2e4ef2d1de63bc295d
WevTop <danieldent20@gmail.com>
2020-04-21 08:49:47 +0000

    Merged in price-dynamic-tier (pull request #15)
    Add dynamic prices from api for Tier
    
    Approved-by: Simen Johnsrud

--- functions/src/mobilityPrices.ts
+++ functions/src/mobilityPrices.ts
@@ -1,37 +1,72 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as request from "superagent";
+
+const MILLIS_PER_MINUTE = 60000;
+const EPOCH = new Date(0);
 
 interface priceText {
-    nob: string,
-    nno: string,
-    eng: string
+    nob: string;
+    nno: string;
+    eng: string;
 }
 
 interface priceTexts {
-    [key: string]: priceText
+    [key: string]: priceText;
+}
+
+interface ScooterPrice {
+    startPrice: number;
+    pricePerMinute: number;
+}
+
+const defaultPrice = {
+    startPrice: 10,
+    pricePerMinute: 2.5
+};
+
+const tierApiUrl = "https://platform.tier-services.io/v1/pricing";
+let tier = {
+    prices: {
+        startPrice: defaultPrice.startPrice,
+        pricePerMinute: defaultPrice.pricePerMinute
+    },
+    lastFetched: EPOCH
+};
+
+const formatDecimals = (price: number, locale: "en" | "no") => {
+    const [integer, fraction] = price.toString().split(".");
+    return fraction
+        ? [integer, fraction.padEnd(2, "0")].join(locale === "en" ? "." : ",")
+        : price;
+};
+
+function priceToText(startPrice: number, pricePerMinute: number) {
+    return {
+        nob: `${startPrice} kr i oppstart + ${formatDecimals(
+            pricePerMinute,
+            "no"
+        )} kr per min`,
+        nno: `${startPrice} kr i oppstart + ${formatDecimals(
+            pricePerMinute,
+            "no"
+        )} kr per min`,
+        eng: `NOK ${startPrice} to unlock + NOK ${formatDecimals(
+            pricePerMinute,
+            "en"
+        )} per min`
+    };
 }
 
 const scooterPrices: priceTexts = {
-    voi: {
-        nob: '10 kr i oppstart + 2,50 kr per min',
-        nno: '10 kr i oppstart + 2,50 kr per min',
-        eng: 'NOK 10 to unlock + NOK 2.50 per min'
-    },
-    tier: {
-        nob: '10 kr i oppstart + 2,50 kr per min',
-        nno: '10 kr i oppstart + 2,50 kr per min',
-        eng: 'NOK 10 to unlock + NOK 2.50 per min'
-    },
-    zvipp: {
-        nob: '10 kr i oppstart + 3 kr per min',
-        nno: '10 kr i oppstart + 3 kr per min',
-        eng: 'NOK 10 to unlock + NOK 3 per min'
-    }
+    voi: priceToText(defaultPrice.startPrice, defaultPrice.pricePerMinute),
+    tier: priceToText(defaultPrice.startPrice, defaultPrice.pricePerMinute),
+    zvipp: priceToText(defaultPrice.startPrice, 3)
 };
 
 const osloBergenTrondheim: priceText = {
-    nob: '399 kr for sesongpass / 49 kr for dagspass',
-    nno: '399 kr for sesongpass / 49 kr for dagspass',
-    eng: 'NOK 399 for season pass / NOK 49 for day pass'
+    nob: "399 kr for sesongpass / 49 kr for dagspass",
+    nno: "399 kr for sesongpass / 49 kr for dagspass",
+    eng: "NOK 399 for season pass / NOK 49 for day pass"
 };
 
 const cityBikePrices: priceTexts = {
@@ -39,17 +74,74 @@ const cityBikePrices: priceTexts = {
     bergen: osloBergenTrondheim,
     trondheim: osloBergenTrondheim,
     drammen: {
-        nob: '130 kr for sesongpass',
-        nno: '130 kr for sesongpass',
-        eng: 'NOK 130 for season pass'
+        nob: "130 kr for sesongpass",
+        nno: "130 kr for sesongpass",
+        eng: "NOK 130 for season pass"
     },
     lillestrom: {
-        eng: 'NOK 50 for season pass / NOK 10 for 3-day pass',
-        nob: '50 kr for sesongpass / 10 kr for 3-dagerskort',
-        nno: 'NOK 50 for sesongpass / NOK 10 for 3-dagarskort'
+        eng: "NOK 50 for season pass / NOK 10 for 3-day pass",
+        nob: "50 kr for sesongpass / 10 kr for 3-dagerskort",
+        nno: "50 kr for sesongpass / 10 kr for 3-dagarskort"
     }
 };
 
-export const prices = functions.region('europe-west1').https.onRequest(async (req, res) => {
-    res.status(200).send({scooters: scooterPrices, cityBikes: cityBikePrices});
-});
+export const prices = functions
+    .region("europe-west1")
+    .https.onRequest(async (req: any, res: any) => {
+        const tierPrices = await tierPrice();
+
+        res.status(200).send({
+            scooters: {
+                ...scooterPrices,
+                tier: priceToText(
+                    tierPrices.startPrice,
+                    tierPrices.pricePerMinute
+                )
+            },
+            cityBikes: cityBikePrices
+        });
+    });
+
+async function tierPrice(zone: string = "OSLO"): Promise<ScooterPrice> {
+    if (toggles().tier === "off") {
+        console.log("Tier is toggled off");
+        return defaultPrice;
+    }
+    if (priceFetchedWithinLast30Minutes()) {
+        return tier.prices;
+    }
+
+    let url: string = `${tierApiUrl}?zoneId=${zone}`;
+
+    try {
+        const tierResponse: request.Response = await request
+            .get(`${url}`)
+            .set("x-api-key", functions.config().tier.api.key);
+        const {
+            attributes: { rentalStartPrice, rentalRunningPricePerMinute }
+        } = JSON.parse(tierResponse.text).data;
+
+        tier = {
+            prices: {
+                startPrice: rentalStartPrice,
+                pricePerMinute: rentalRunningPricePerMinute
+            },
+            lastFetched: new Date()
+        };
+        return tier.prices;
+    } catch (err) {
+        console.error(err);
+        return defaultPrice;
+    }
+}
+
+function priceFetchedWithinLast30Minutes() {
+    return (
+        new Date().getTime() <
+        tier.lastFetched.getTime() + 30 * MILLIS_PER_MINUTE
+    );
+}
+
+function toggles() {
+    return functions.config().toggles || {};
+}

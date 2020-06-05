41ecbbfb648db2e9627e76fcaef994d237ee6c0d
WevTop <danieldent20@gmail.com>
2020-06-05 11:15:50 +0000

    Merged in pris-kolumbus (pull request #26)
    Add price for Kolumbus bysykkel
    
    Approved-by: Simen Johnsrud

--- functions/src/utils/constants.ts
+++ functions/src/utils/constants.ts
@@ -1,14 +1,18 @@
-import {priceText, priceTexts, ScooterPrice} from "./interfaces";
-import {formatPriceToText} from "./formatters";
+import { priceText, priceTexts, ScooterPrice } from "./interfaces";
+import { formatPriceToText } from "./formatters";
 
 export const CLIENT_HEADER_NAME: string = "ET-Client-Name";
 export const CLIENT_ENTUR: string = "entur-client";
-export const tierApiUrl = "https://platform.tier-services.io/v1/vehicle?zoneId=OSLO";
+export const tierApiUrl =
+    "https://platform.tier-services.io/v1/vehicle?zoneId=OSLO";
 export const tierPricingApiUrl = "https://platform.tier-services.io/v1/pricing";
-export const voiApiUrlOslo = "https://mds.voiapp.io/v1/gbfs/en/27/free_bike_status";
-export const voiApiUrlTrondheim = "https://mds.voiapp.io/v1/gbfs/en/196/free_bike_status";
+export const voiApiUrlOslo =
+    "https://mds.voiapp.io/v1/gbfs/en/27/free_bike_status";
+export const voiApiUrlTrondheim =
+    "https://mds.voiapp.io/v1/gbfs/en/196/free_bike_status";
 export const voiSessionKeyUrl = "https://mds.voiapp.io/token";
-export const zvippApiUrlDrammen = "https://zvipp-api.joyridecity.bike/gbfs/en/free_bike_status.json?operator_id=67";
+export const zvippApiUrlDrammen =
+    "https://zvipp-api.joyridecity.bike/gbfs/en/free_bike_status.json?operator_id=67";
 export const TIER_MAX_RANGE = 30000;
 export const MILLIS_PER_MINUTE = 60000;
 
@@ -30,6 +34,11 @@ export const cityBikeStaticPrices: priceTexts = {
         eng: "NOK 50 for season pass / NOK 10 for 3-day pass",
         nob: "50 kr for sesongpass / 10 kr for 3-dagerskort",
         nno: "50 kr for sesongpass / 10 kr for 3-dagarskort"
+    },
+    kolumbus: {
+        eng: "Free for one hour with a valid ticket",
+        nob: "Gratis i én time med gyldig billett",
+        nno: "Gratis i éin time med gyldig billett"
     }
 };
 export const defaultScooterPrice: ScooterPrice = {
@@ -37,7 +46,13 @@ export const defaultScooterPrice: ScooterPrice = {
     pricePerMinute: 2.5
 };
 export const scooterPrices: priceTexts = {
-    voi: formatPriceToText(defaultScooterPrice.startPrice, defaultScooterPrice.pricePerMinute),
-    tier: formatPriceToText(defaultScooterPrice.startPrice, defaultScooterPrice.pricePerMinute),
+    voi: formatPriceToText(
+        defaultScooterPrice.startPrice,
+        defaultScooterPrice.pricePerMinute
+    ),
+    tier: formatPriceToText(
+        defaultScooterPrice.startPrice,
+        defaultScooterPrice.pricePerMinute
+    ),
     zvipp: formatPriceToText(defaultScooterPrice.startPrice, 3)
 };

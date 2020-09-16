fc3c44fc6da6a9cee9275cbedfc1abcbaf1265cf
WevTop <danieldent20@gmail.com>
2020-09-16 14:21:27 +0200

    Cache promise instead, which simplifies hydration process

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -7,59 +7,58 @@ const VEHICLE_CACHE_TTL_IN_MS = 10000
 type VehicleCache = {
     lastFetched: Date | null
     isFetching: boolean
-    vehicles: Vehicle[] | null
+    fetcherPromise: Promise<Vehicle[]> | null
 }
 
 const cache: Record<Operator, VehicleCache> = {
-    [Operator.VOI]: { lastFetched: null, isFetching: false, vehicles: null },
-    [Operator.TIER]: { lastFetched: null, isFetching: false, vehicles: null },
-    [Operator.ZVIPP]: { lastFetched: null, isFetching: false, vehicles: null },
-    [Operator.LIME]: { lastFetched: null, isFetching: false, vehicles: null },
+    [Operator.VOI]: {
+        lastFetched: null,
+        isFetching: false,
+        fetcherPromise: null,
+    },
+    [Operator.TIER]: {
+        lastFetched: null,
+        isFetching: false,
+        fetcherPromise: null,
+    },
+    [Operator.ZVIPP]: {
+        lastFetched: null,
+        isFetching: false,
+        fetcherPromise: null,
+    },
+    [Operator.LIME]: {
+        lastFetched: null,
+        isFetching: false,
+        fetcherPromise: null,
+    },
 }
 
-export function getCachedScooters(
+const expired = (lastFetched: Date | null) =>
+    lastFetched === null ||
+    lastFetched!.getTime() + VEHICLE_CACHE_TTL_IN_MS < new Date().getTime()
+
+export async function getCachedScooters(
     operator: Operator,
     fetchVehicles: () => Promise<Vehicle[]>,
 ) {
     const vehicleCache = cache[operator]
 
-    if (vehicleCache.vehicles === null) {
-        if (vehicleCache.isFetching) {
-            return []
-        }
-
+    if (
+        !vehicleCache.fetcherPromise ||
+        (expired(vehicleCache.lastFetched) && !vehicleCache.isFetching)
+    ) {
         vehicleCache.isFetching = true
-        return fetchVehicles()
+        vehicleCache.fetcherPromise = fetchVehicles()
             .then((freshVehicles) => {
-                vehicleCache.vehicles = freshVehicles || []
-                vehicleCache.lastFetched = new Date()
                 vehicleCache.isFetching = false
+                vehicleCache.lastFetched = new Date()
                 return freshVehicles
             })
             .catch((e) => {
                 logError(operator, `error hydrating vehicle cache: ${e}`)
-                vehicleCache.isFetching = false
                 return []
             })
     }
 
-    if (
-        vehicleCache.lastFetched!.getTime() + VEHICLE_CACHE_TTL_IN_MS <
-            new Date().getTime() &&
-        !vehicleCache.isFetching
-    ) {
-        vehicleCache.isFetching = true
-        fetchVehicles()
-            .then((freshVehicles) => {
-                vehicleCache.vehicles = freshVehicles || []
-                vehicleCache.lastFetched = new Date()
-                vehicleCache.isFetching = false
-            })
-            .catch((e) => {
-                logError(operator, `error refetching vehicle cache: ${e}`)
-                vehicleCache.isFetching = false
-            })
-    }
-
-    return vehicleCache.vehicles
+    return await vehicleCache.fetcherPromise!
 }

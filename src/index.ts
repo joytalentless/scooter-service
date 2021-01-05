5a1ee0de35637b88d68e011ea5bf618b715e7d93
WevTop <danieldent20@gmail.com>
2021-01-05 13:32:43 +0100

    Remove old boltoslo endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { scooters } from './scooters'
 import { prices } from './mobilityPrices'
-import * as boltoslo from './boltoslo'
 import * as gbfs from './gbfs'
 
-export { scooters, prices, boltoslo, gbfs }
+export { scooters, prices, gbfs }
